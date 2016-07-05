d58fc43c1f5fe72c73d01faa71ab2a71064dd5d4
Glenn Goodrich <glenn.goodrich@gmail.com>
2016-07-04 22:27:25 -0400

    Change tests for Rails 5

--- test/controllers/stories_controller_test.rb
+++ test/controllers/stories_controller_test.rb
@@ -1,9 +1,40 @@
 require 'test_helper'
 
 class StoriesControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test "gets stories" do
     get stories_path
     assert_response :success
-    puts response.body
+    assert response.body.include?("A random link")
+  end
+
+  test "gets new story form" do
+    get new_story_path
+    assert_response :success
+  end
+
+  test "new shows new form" do
+    get new_story_path
+    assert_select 'form div', count: 2
+  end
+
+  test "adds a story" do
+    assert_difference 'Story.count' do
+      post stories_path, params: {
+        story: {
+          name: 'test story',
+          link: 'http://www.test.com/'
+        }
+      }
+    end
+    assert_redirected_to stories_path
+    assert_not_nil flash[:notice]
+  end
+
+  test "rejects when missing story attribute" do
+    assert_no_difference 'Story.count' do
+      post stories_path, params: {
+        story: { name: 'story without a link' }
+      }
+    end
   end
 end
