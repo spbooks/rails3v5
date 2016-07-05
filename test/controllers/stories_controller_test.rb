1f0adcbd9af9a3ae6fbc4e21335187401f2f793f
Glenn Goodrich <glenn.goodrich@gmail.com>
2016-07-04 21:57:00 -0400

    tests

--- test/controllers/stories_controller_test.rb
+++ test/controllers/stories_controller_test.rb
@@ -2,7 +2,8 @@ require 'test_helper'
 
 class StoriesControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get '/stories'
+    get stories_path
     assert_response :success
+    puts response.body
   end
 end
