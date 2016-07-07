2e1ac70a37ee2829bd1dbd0ccf4c62689a9268cb
Glenn Goodrich <glenn.goodrich@gmail.com>
2016-07-07 09:36:40 -0600

    User stuff

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160706182222) do
+ActiveRecord::Schema.define(version: 20160706195108) do
 
   create_table "stories", force: :cascade do |t|
     t.string   "name"
     t.string   "link"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "password_digest"
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
   create_table "votes", force: :cascade do |t|
     t.integer  "story_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
 end
