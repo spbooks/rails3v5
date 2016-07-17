d09973635473403ec68fd71db79906b5d31e3058
Glenn Goodrich <glenn.goodrich@gmail.com>
2016-07-17 18:48:19 -0400

    Rename login_required

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :current_user
 
-
   protected
 
   def current_user
@@ -10,7 +9,7 @@ class ApplicationController < ActionController::Base
     @current_user = User.where(id: session[:user_id]).first
   end
 
-  def login_required
+  def ensure_login
     return true if logged_in?
     session[:return_to] = request.fullpath
     redirect_to new_session_path and return false
