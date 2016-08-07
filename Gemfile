b769ad24fdc683b5259668db5495691ca9a27058
Glenn Goodrich <glenn.goodrich@gmail.com>
2016-08-07 13:46:55 -0400

    Move sessions to the db

--- Gemfile
+++ Gemfile
@@ -25,6 +25,8 @@ gem 'jbuilder', '~> 2.5'
 # Use ActiveModel has_secure_password
 gem 'bcrypt', '~> 3.1.7'
 
+gem 'activerecord-session_store'
+
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160723173642) do
+ActiveRecord::Schema.define(version: 20160807174446) do
+
+  create_table "sessions", force: :cascade do |t|
+    t.string   "session_id", null: false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.index ["session_id"], name: "index_sessions_on_session_id", unique: true
+    t.index ["updated_at"], name: "index_sessions_on_updated_at"
+  end
 
   create_table "stories", force: :cascade do |t|
     t.string   "name"
